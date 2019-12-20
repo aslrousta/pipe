799f249af515f84f0e2eaa8064c0df072e421483
Ali AslRousta <aslrousta@gmail.com>
2019-12-20 10:29:22 +0330

    Simple pipeline

--- pipe.go
+++ pipe.go
@@ -1,15 +1,86 @@
 package pipe
 
-// Func is the func type for the pipeline result.
-type Func func() error
+import (
+	"fmt"
+	"reflect"
+)
 
-func emptyFunc() error { return nil }
+// Pipeline is the func type for the pipeline result.
+type Pipeline func() error
 
-// Pipe accepts several funcs and creates a pipeline.
-func Pipe(fs ...interface{}) Func {
-	if len(fs) == 0 {
-		return emptyFunc
+func empty() error { return nil }
+
+// Pipe accepts zero or more funcs fs and creates a pipeline.
+//
+// A pipeline syncs outputs and inputs of consequent funcs together, such that
+// the output of i'th func is the input of (i+1)'th func. Each func can accept
+// zero or one input argument and return zero or one value with an optional
+// error.
+//
+// The first func is called a generator and can accept no input arguments but
+// only return a value (optionally, with an error), and the last func is called
+// a sink which only accepts an input argument and returns no value except an
+// optional error.
+//
+// If a func in the pipeline fails with an error during the invocation, the pipe
+// is broken immediately and the invocation returns an error.
+func Pipe(fs ...interface{}) Pipeline {
+	return func() (err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				err = fmt.Errorf("pipeline panicked: %v", r)
+			}
+		}()
+
+		var inputs []reflect.Value
+		for i, f := range fs {
+			if f == nil {
+				return fmt.Errorf("%s arg is nil", ord(i))
+			}
+
+			t := reflect.TypeOf(f)
+			if t.Kind() != reflect.Func {
+				return fmt.Errorf("%s arg is not a func", ord(i))
+			}
+
+			if t.NumIn() != len(inputs) {
+				return fmt.Errorf(
+					"%s func accepts %d args, %d passed",
+					ord(i), t.NumIn(), len(inputs),
+				)
+			}
+
+			outputs := reflect.ValueOf(f).Call(inputs)
+
+			inputs = inputs[:0]
+			for _, output := range outputs {
+				if e, ok := output.Interface().(error); ok {
+					if err != nil {
+						err = fmt.Errorf("%s func failed: %w", ord(i), e)
+						return
+					}
+				} else {
+					inputs = append(inputs, output)
+				}
+			}
+		}
+
+		return
 	}
+}
 
-	return nil
+func ord(index int) string {
+	order := index + 1
+	switch {
+	case order > 10 && order < 20:
+		return fmt.Sprintf("%dth", order)
+	case order%10 == 1:
+		return fmt.Sprintf("%dst", order)
+	case order%10 == 2:
+		return fmt.Sprintf("%dnd", order)
+	case order%10 == 3:
+		return fmt.Sprintf("%drd", order)
+	default:
+		return fmt.Sprintf("%dth", order)
+	}
 }
--- pipe_test.go
+++ pipe_test.go
@@ -11,7 +11,30 @@ func TestPipe(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		pipe := Pipe()
 
-		NotNil(t, pipe, "pipe is nil")
-		NoError(t, pipe(), "unexpected error")
+		NotNil(t, pipe)
+		NoError(t, pipe())
+	})
+
+	t.Run("One", func(t *testing.T) {
+		executed := false
+		pipe := Pipe(func() { executed = true })
+
+		NotNil(t, pipe)
+		NoError(t, pipe())
+		Equal(t, executed, true)
+	})
+
+	t.Run("More", func(t *testing.T) {
+		var result int
+
+		pipe := Pipe(
+			func() int { return 10 },
+			func(n int) int { return n + 12 },
+			func(n int) { result = n / 2 },
+		)
+
+		NotNil(t, pipe)
+		NoError(t, pipe())
+		Equal(t, result, 11)
 	})
 }
